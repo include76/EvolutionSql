532a853be0368159c9b6fc9f8ac7edada66dc22e
Locke Duan <lockeduan@outlook.com>
2019-10-28 21:18:16 +0800

    update

---
+++ Evolution.Sql/EvolutionSql.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Evolution.Sql
+{
+    public static class EvolutionSql
+    {
+        public static Command RunProcedure(this DbConnection dbConnection, string procedureName, object parameters)
+        {
+
+        }
+
+        public static Command RunSql(this DbConnection dbConnection, string sql, object parameters)
+        {
+
+        }
+    }
+}
