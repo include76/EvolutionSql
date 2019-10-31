85df4536b4bf4af2d676223bd9792ecf64a126dc
Locke Duan <lockeduan@outlook.com>
2019-10-31 17:54:03 +0800

    update pgsql typemap

--- Evolution.Sql/Command/Command.PgSql.cs
+++ Evolution.Sql/Command/Command.PgSql.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Evolution.Sql.Command
 {
-    public class PgSqlCommand: AbstractCommand
+    public class PgSqlCommand : AbstractCommand
     {
         public PgSqlCommand() : base()
         {
+            DbDataTypeDbTypeMap = new Dictionary<string, DbType>()
+            {
+                //Numeric
+                {"smallint", DbType.Int16  },
+                {"integer",  DbType.Int32 },
+                {"bigint",  DbType.Int64 },
+                {"decimal", DbType.Decimal },
+                {"numeric", DbType.Decimal },
+                {"real", DbType.Single  },
+                {"double precision", DbType.Double },
+                {"smallserial", DbType.UInt16 },
+                {"serial", DbType.UInt32 },
+                {"bigserial", DbType.UInt64 },
+                //Money
+                {"money", DbType.Decimal },
+                //Character
+                {"character varying", DbType.String },
+                {"varchar",  DbType.String},
+                {"character",  DbType.StringFixedLength},
+                {"char",  DbType.StringFixedLength },
+                {"text", DbType.String  },
+                //Binary
+                {"bytea",  DbType.Binary},
+                //Date/Time
+                {"timestamp", DbType.DateTime2},
+                {"date", DbType.Date  },
+                {"time", DbType.Time },
+                //{"interval" , DbType.}
 
+            };
         }
     }
 }
